17674fc095ca3fbb0082bf24ce63528090fc8301
ethancoyle7 <77364000+ethancoyle7@users.noreply.github.com>
2021-03-09 10:36:21 -0600

    Create source.cpp

---
+++ nestedloops/forloop/source.cpp
@@ -0,0 +1,16 @@
+
+using System;  
+  
+class Ethan Coyle
+{ 
+      
+  public static void Main()  
+  {  
+      
+    // nested for loop   
+    // printing my name  
+    for(int i = 0; i < 5; i++)  
+        for(int j = 1; j < i; j++)  
+            Console.WriteLine("Ethan Coyle!!");  
+  }  
+}
