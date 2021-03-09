8a1e68850efca6f351eef65652ad6e245c496fa1
ethancoyle7 <77364000+ethancoyle7@users.noreply.github.com>
2021-03-09 10:26:38 -0600

    Create main.cpp

---
+++ helloworld/main.cpp
@@ -0,0 +1,21 @@
+using System; // like using namespace
+namespace EthanCoyle// namespace named ethan
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        // main accepting arguments
+        {
+          //first string
+            string message = "Hello World!!";
+
+          //second string
+            string message2 = "My name is Ethan Coyle!!";
+          //display first string
+            Console.WriteLine(message);
+          //display second string
+            Console.WriteLine(message2);
+        }
+    }
+    
+}//end of program
